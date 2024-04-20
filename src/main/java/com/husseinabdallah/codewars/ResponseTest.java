9a92c4f7768c3bc186575a5711382ca1a9771f04
mishobo <husseinabdalla287@gmail.com>
2024-04-20 21:03:34 +0300

    json object

--- src/main/java/com/husseinabdallah/codewars/ResponseTest.java
+++ src/main/java/com/husseinabdallah/codewars/ResponseTest.java
@@ -1,16 +1,19 @@
 package com.husseinabdallah.codewars;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResponseTest {
 
     public static void main (String[] args) {
+        Gson gson = new Gson();
         Person person = new Person();
 
         ApiResult<Person> api = new ApiResult<>(person, "successful", true);
 
-        System.out.println(api);
+        System.out.println(gson.toJson(api));
 
         Person person1 = new Person();
         person1.name = "Abdallah Mishobo";
@@ -24,7 +27,7 @@ public class ResponseTest {
 
         ApiResult<People> api1 = new ApiResult<>(people, "successful", true);
 
-        System.out.println(api1);
+        System.out.println(gson.toJson(api1));
 
 
 
